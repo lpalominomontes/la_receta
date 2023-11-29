59faed1ff8f286a248526a239b2cf985a10b7530
lpalominomontes <lpalominomontes@hotmail.com>
2023-11-29 13:50:08 -0300

    MainActivity and RegisterActivity

--- app/src/main/java/com/example/lareceta/MainActivity.kt
+++ app/src/main/java/com/example/lareceta/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.lareceta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lareceta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        registerActivity()
+    }
+
+    private fun registerActivity() {
+        binding.btnRegistrar.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
     }
 }
